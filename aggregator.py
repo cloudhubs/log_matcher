835669560ca42b610865c87e7ccb8fc67a3e2367
JacksonRaffety <jwraffety@gmail.com>
2020-07-09 10:35:54 -0700

    aggregator template

---
+++ aggregator.py
@@ -0,0 +1,8 @@
+# Definition: convert input data to intermediary step
+def aggregate_logs():
+    return 0
+
+
+# Definition: convert intermediary data to output step
+def aggregate_data():
+    return 0
