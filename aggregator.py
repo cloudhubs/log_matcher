d1f60b6e21ca7d72b80ceb80edabf4bb25def21d
JacksonRaffety <jwraffety@gmail.com>
2020-07-09 10:39:52 -0700

    function and test stubs

--- aggregator.py
+++ aggregator.py
@@ -1,8 +1,8 @@
 # Definition: convert input data to intermediary step
-def aggregate_logs():
+def aggregate_logs(logs_a, logs_b):
     return 0
 
 
 # Definition: convert intermediary data to output step
-def aggregate_data():
+def aggregate_data(data):
     return 0
