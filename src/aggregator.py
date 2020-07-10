9e5f6c21cc54c5f26749509c453417bfd7f6be88
JacksonRaffety <jwraffety@gmail.com>
2020-07-10 08:31:11 -0700

    better function headers

--- src/aggregator.py
+++ src/aggregator.py
@@ -1,8 +1,3 @@
-# Definition: convert input data to intermediary step
-def aggregate_logs(logs_a, logs_b):
-    return 0
-
-
 # Definition: convert intermediary data to output step
 def aggregate_data(data):
     return 0
--- tests/tests.py
+++ tests/tests.py
@@ -1,15 +1,15 @@
 import pytest
-from src import file_reader, aggregator
+from src import file_reader, aggregator, log_matcher
 
 
-def test_aggregate_logs():
+def test_match_logs():
     input_pipeline = file_reader.read_json_file("./test_data/test_data_pipeline.txt")
     input_agg = file_reader.read_json_file("./test_data/test_data_aggregator.txt")
     output = file_reader.read_json_file("./test_data/test_data_intermediary.txt")
 
-    aggregation = aggregator.aggregate_logs(input_pipeline, input_agg)
+    match_data = log_matcher.match_logs(input_pipeline, input_agg)
 
-    assert aggregation == output
+    assert match_data == output
 
 
 def test_aggregate_data():
