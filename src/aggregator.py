21ee47de1490ea12db27a86843ee4a29d7f7aa1a
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-07-10 12:51:39 -0500

    Added Output object class type

--- src/aggregator.py
+++ src/aggregator.py
@@ -1,3 +1,11 @@
+class Output:
+    signature = ""
+    unique_matches=0
+    matches=0
+    unique_values=0
+    total_values=0
+
 # Definition: convert intermediary data to output step
 def aggregate_data(data):
+
     return 0
