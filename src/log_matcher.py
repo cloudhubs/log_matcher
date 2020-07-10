5f568e64e2e2eddf3b319cf95e40221cdbe1a830
JacksonRaffety <jwraffety@gmail.com>
2020-07-10 11:47:29 -0700

    added matching

--- src/log_matcher.py
+++ src/log_matcher.py
@@ -1,14 +1,33 @@
 from src import file_reader
 
 
+# Name: match_logs
+# Input: 2 log files in JSON format after extracting the singatures
+#        examples are test_data/test_data_aggregator.txt and test_data/test_data_pipeline.txt
+# Output: Form data on each regex and individual field on number of matches and occurrences of data
+#          example is test_data/test_data/intermediary.txt
 # Definition: convert input data to intermediary step
 def match_logs(logs_a, logs_b):
-    match_data_a = {}
-    match_data_b = {}
-    match_data_aggregate = {}
+    match_data_b = get_log_data(logs_a)
+    match_data_a = get_log_data(logs_b)
 
-    match_data_a = get_log_data(logs_a)
-    match_data_b = get_log_data(logs_b)
+    # go through each log file, adding to the aggregate and incrementing matches
+    match_data_aggregate = match_data_a
+    for regex in list(match_data_b.keys()):
+        # new, unique regex to the aggregation (not found in a)
+        if regex not in match_data_aggregate:
+            match_data_aggregate[regex] = match_data_b[regex]
+        else:
+            # existing regex, add values and update
+            for entry in list(match_data_b[regex].keys()):
+                # new entry
+                if entry not in match_data_aggregate[regex]:
+                    match_data_aggregate[regex][entry] = match_data_b[regex][entry]
+                else:
+                    # found a match
+                    match_data_aggregate[regex][entry][0] = match_data_aggregate[regex][entry][1] * \
+                                                            match_data_b[regex][entry][1]
+                    match_data_aggregate[regex][entry][1] += match_data_b[regex][entry][1]
 
     return 0
 
@@ -41,6 +60,8 @@ def get_log_data(log_file):
     return match_data
 
 
+"""
 if __name__ == "__main__":
     match_logs(file_reader.read_json_file("../test_data/test_data_aggregator.txt"),
                file_reader.read_json_file("../test_data/test_data_pipeline.txt"))
+"""
