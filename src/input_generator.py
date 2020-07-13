1876af715dd31ad3a3fcf5ba83002edac563e2a9
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-07-13 13:31:07 -0500

    Set up input script for large test data

---
+++ src/input_generator.py
@@ -0,0 +1,20 @@
+
+class BigObj:
+    line_type=""
+    regex_items=[]
+
+class Regex:
+    regex=""
+    line_items=[]
+
+class Line:
+    value=""
+    line_numbers=[]
+
+def generate_big_obj(file_path):
+    objects=[]
+
+    return objects
+
+if __name__ == __main__:
+    print(generate_big_obj("test_data/pipelineout.txt"))
