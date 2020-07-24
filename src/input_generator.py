3fb3f7be0cd7b1dac99c93673558297445f7e854
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-07-24 10:47:52 -0500

    Remove block of code that makes the array a string

--- src/input_generator.py
+++ src/input_generator.py
@@ -113,7 +113,7 @@ def get_regex(value):
 obj = generate_big_obj("../test_data/aggregatorout.txt")
 
 # add this block for more readable output
-
+"""
 for value in obj:
     for val in obj[value]:
         for va in obj[value][val]:
@@ -122,7 +122,7 @@ for value in obj:
                 for num in obj[value][val][va][v]:
                     list += str(num) + ","
                 list = list[0:len(list)-1] + "]"
-                obj[value][val][va][v] = list
+                obj[value][val][va][v] = list"""
 
 with open("../test_data/test_output.txt", "w") as file:
     file.write(json.dumps(obj, indent=3))
